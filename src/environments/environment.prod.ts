79c388eee64f5fb8f2af37cd291c6281a5009ddb
moises <moimigueldev@gmail.com>
2020-03-01 13:17:57 -0600

    deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD3ZFcj5Kui8JLAH3a_cXdL8eXbXlX9kPY",
+    authDomain: "fasttype-c3b85.firebaseapp.com",
+    databaseURL: "https://fasttype-c3b85.firebaseio.com",
+    projectId: "fasttype-c3b85",
+    storageBucket: "fasttype-c3b85.appspot.com",
+    messagingSenderId: "519984368022",
+    appId: "1:519984368022:web:8fc0338531cc17c91a75b7",
+    measurementId: "G-2X14W346TZ"
+  }
 };
